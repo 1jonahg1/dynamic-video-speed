c51e339bf29299ddb082d9afe9a22d94ee4c4abe
Jonah <1jonahg1@gmail.com>
2022-09-08 00:05:01 +0300

    Update dynamic_speed.py

--- dynamic_speed.py
+++ dynamic_speed.py
@@ -26,14 +26,6 @@ def splitClips():
 
 clipList = splitClips()
 
-"""
-clip1 = clip.subclip(0, 20)
-clip2 = clip.subclip(20, 40)
-clip3 = clip.subclip(40, 60)
-clip1.write_videofile("dynamic_test_output/my_video_1.mp4")
-clip2.write_videofile("dynamic_test_output/my_video_2.mp4")
-clip3.write_videofile("dynamic_test_output/my_video_3.mp4")
-"""
 
 def get_words(name):
     command2mp3 = "ffmpeg -i dynamic_test_output/{}.mp4 dynamic_test_output/{}.mp3".format(name,name)
@@ -62,16 +54,3 @@ def update_speeds():
 
 update_speeds()
 
-
-"""
-#296 in 100 sec -> 178wpm --> I want 300 wpm
-words1 = get_words("my_video_1")
-words2 = get_words("my_video_2")
-words3 = get_words("my_video_3")
-
-videos = [
-  Video(speed=300/(words1*3), path="dynamic_test_output/my_video_1.mp4"), # change 3 to 60/video-length
-  Video(speed=300/(words2*3), path="dynamic_test_output/my_video_2.mp4"), #change 300 to wanted speed
-  Video(speed=300/(words3*3), path="dynamic_test_output/my_video_3.mp4"),
-]
-"""
