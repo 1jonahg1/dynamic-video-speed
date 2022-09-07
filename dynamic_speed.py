135f945d28e3d0bf68f6f26bc2ee4c5264170fd5
Jonah <1jonahg1@gmail.com>
2022-09-08 00:03:43 +0300

    Finished tests to split into subclips by duration

--- dynamic_speed.py
+++ dynamic_speed.py
@@ -3,18 +3,37 @@ import speech_recognition as sr
 from adjustPitchSpeed import Video, concatenate_videos
 import os
 import ffmpeg
+import subprocess
+import math
 
-# loading video 
+SEGMENT_DURATION = 30
+WORDS_PER_MINUTE = 300
+
+# loading video
 clip = VideoFileClip("Python.mp4")
+duration = clip.duration
+videoSegments = math.ceil(duration / SEGMENT_DURATION)
+lastVideoDuration = duration % SEGMENT_DURATION
 
-#TODO - change to loop to break video into pieces, based on length of video
+def splitClips():
+  clips = []
+  for i in range(videoSegments-1):
+    clips.append(clip.subclip((i)*SEGMENT_DURATION, (i+1)*SEGMENT_DURATION))
+  clips.append(clip.subclip(duration-lastVideoDuration, duration))
+  for index, video in enumerate(clips):
+    video.write_videofile("dynamic_test_output/my_video_{}.mp4".format(index))
+  return clips
+
+clipList = splitClips()
+
+"""
 clip1 = clip.subclip(0, 20)
 clip2 = clip.subclip(20, 40)
 clip3 = clip.subclip(40, 60)
-
 clip1.write_videofile("dynamic_test_output/my_video_1.mp4")
 clip2.write_videofile("dynamic_test_output/my_video_2.mp4")
 clip3.write_videofile("dynamic_test_output/my_video_3.mp4")
+"""
 
 def get_words(name):
     command2mp3 = "ffmpeg -i dynamic_test_output/{}.mp4 dynamic_test_output/{}.mp3".format(name,name)
@@ -27,18 +46,32 @@ def get_words(name):
     text = (r.recognize_google(audio)).split()
     print("!!!!Length of text!!!! \n" + str(len(text)))
     return len(text)
+  
+def update_speeds():
+  videos = []
+  for index in range(len(clipList)-1):
+    words = get_words("my_video_{}".format(index))
+    adjustSpeed = WORDS_PER_MINUTE/(words*(60/SEGMENT_DURATION))
+    videos.append(Video(speed=adjustSpeed, path="dynamic_test_output/my_video_{}.mp4".format(index)))
+  #edit last segment
+  words = get_words("my_video_{}".format(index+1))
+  adjustSpeed = WORDS_PER_MINUTE/(words*(60/lastVideoDuration))
+  videos.append(Video(speed=adjustSpeed, path="dynamic_test_output/my_video_{}.mp4".format(index+1)))
+  #update speeds 
+  concatenate_videos(videos=videos, output_file=f"dynamic_test_output/final_output_video.mp4")
 
+update_speeds()
+
+
+"""
 #296 in 100 sec -> 178wpm --> I want 300 wpm
-
 words1 = get_words("my_video_1")
 words2 = get_words("my_video_2")
 words3 = get_words("my_video_3")
 
-#update speeds 
 videos = [
   Video(speed=300/(words1*3), path="dynamic_test_output/my_video_1.mp4"), # change 3 to 60/video-length
   Video(speed=300/(words2*3), path="dynamic_test_output/my_video_2.mp4"), #change 300 to wanted speed
   Video(speed=300/(words3*3), path="dynamic_test_output/my_video_3.mp4"),
 ]
-
-concatenate_videos(videos=videos, output_file=f"dynamic_test_output/final_output_video.mp4")
+"""
