3ba75cd4cea3969fc559a14838df20d60f12b772
Jonah <1jonahg1@gmail.com>
2022-10-02 12:09:56 +0300

    connected tkinter interface

--- dynamic_speed.py
+++ dynamic_speed.py
@@ -7,50 +7,51 @@ import subprocess
 import math
 
 SEGMENT_DURATION = 30
-WORDS_PER_MINUTE = 300
+#WORDS_PER_MINUTE = 300
 
 # loading video
-clip = VideoFileClip("Python.mp4")
-duration = clip.duration
-video_segments = math.ceil(duration / SEGMENT_DURATION)
-last_video_duration = duration % SEGMENT_DURATION
+class VideoEdit():
+  def __init__(self, video_path, input_language, words_per_minute):
+    self.clip = VideoFileClip(video_path)  
+    self.duration = self.clip.duration
+    self.video_segments = math.ceil(self.duration / SEGMENT_DURATION)
+    self.last_video_duration = self.duration % SEGMENT_DURATION
+    self.language = input_language
+    self.wpm = words_per_minute
 
-def splitClips():
-  clips = []
-  for i in range(video_segments-1):
-    clips.append(clip.subclip((i)*SEGMENT_DURATION, (i+1)*SEGMENT_DURATION))
-  clips.append(clip.subclip(duration-last_video_duration, duration))
-  for index, video in enumerate(clips):
-    video.write_videofile("dynamic_test_output/my_video_{}.mp4".format(index))
-  return clips
+  def splitClips(self):
+    clips = []
+    for i in range(self.video_segments-1):
+      clips.append(self.clip.subclip((i)*SEGMENT_DURATION, (i+1)*SEGMENT_DURATION))
+    clips.append(self.clip.subclip(self.duration-self.last_video_duration, self.duration))
+    for index, video in enumerate(clips):
+      video.write_videofile("dynamic_test_output/my_video_{}.mp4".format(index))
+    return clips
 
-clip_list = splitClips()
-
-
-def get_words(name):
-    command2mp3 = "ffmpeg -i dynamic_test_output/{}.mp4 dynamic_test_output/{}.mp3".format(name,name)
-    command2wav = "ffmpeg -i dynamic_test_output/{}.mp3 dynamic_test_output/{}.wav".format(name,name)
-    os.system(command2mp3)
-    os.system(command2wav)
-    r = sr.Recognizer()
-    with sr.AudioFile("dynamic_test_output/{}.wav".format(name)) as source:
-        audio = r.record(source, duration=20) 
-    text = (r.recognize_google(audio)).split()
-    print("!!!!Length of text!!!! \n" + str(len(text)))
-    return len(text)
+  def get_words(self, name):
+      command2mp3 = "ffmpeg -i dynamic_test_output/{}.mp4 dynamic_test_output/{}.mp3".format(name,name)
+      command2wav = "ffmpeg -i dynamic_test_output/{}.mp3 dynamic_test_output/{}.wav".format(name,name)
+      os.system(command2mp3)
+      os.system(command2wav)
+      r = sr.Recognizer()
+      with sr.AudioFile("dynamic_test_output/{}.wav".format(name)) as source:
+          audio = r.record(source, duration=20) 
+      text = (r.recognize_google(audio)).split()
+      print("!!!!Length of text!!!! \n" + str(len(text)))
+      return len(text)
   
-def update_speeds():
-  videos = []
-  for index in range(len(clip_list)-1):
-    words = get_words("my_video_{}".format(index))
-    adjust_speed = WORDS_PER_MINUTE/(words*(60/SEGMENT_DURATION))
-    videos.append(Video(speed=adjust_speed, path="dynamic_test_output/my_video_{}.mp4".format(index)))
-  #edit last segment
-  words = get_words("my_video_{}".format(index+1))
-  adjust_speed = WORDS_PER_MINUTE/(words*(60/last_video_duration))
-  videos.append(Video(speed=adjust_speed, path="dynamic_test_output/my_video_{}.mp4".format(index+1)))
-  #update speeds 
-  concatenate_videos(videos=videos, output_file=f"dynamic_test_output/final_output_video.mp4")
+  def update_speeds(self, clip_list):
+    videos = []
+    for index in range(len(clip_list)-1):
+      words = self.get_words("my_video_{}".format(index))
+      adjust_speed = self.wpm/(words*(60/SEGMENT_DURATION))
+      videos.append(Video(speed=adjust_speed, path="dynamic_test_output/my_video_{}.mp4".format(index))) # TODO - change to temp file
+    #edit last segment
+    words = self.get_words("my_video_{}".format(index+1))
+    adjust_speed = self.wpm/(words*(60/self.last_video_duration))
+    videos.append(Video(speed=adjust_speed, path="dynamic_test_output/my_video_{}.mp4".format(index+1)))
+    #update speeds 
+    concatenate_videos(videos=videos, output_file=f"dynamic_test_output/final_output_video.mp4")
 
-update_speeds()
+
 
--- interface.py
+++ interface.py
@@ -1,52 +1,77 @@
 from tkinter import *
 import tkinter.font as tkFont
+from tkinter import filedialog as fd
+from tkinter.messagebox import showinfo
+import dynamic_speed
+
 
 class MyWindow:
     def __init__(self, window):
+        self.file_path = ''
+        self.picked_file = False
         window.iconbitmap('flash.ico')
         self.title=Label(window, text="Change Lecture Speed to the Ideal Speed For You", fg='black', font=("Helvetica", 14))
         self.title.pack()
         self.f = tkFont.Font(self.title, self.title.cget("font"))
         self.f.configure(underline = True)
         self.title.configure(font=self.f)
-        self.title.place(x=75, y=45)
+        self.title.place(x=75, y=40)
+
+        self.file=Button(window, text="        Choose File        ", fg='white',bg='dark grey',command=self.select_file)
+        self.file.place(x=230, y=80)
 
         self.test=Button(window, text="Test Speed", fg='white',bg='grey',command=self.Test)
-        self.test.place(x=260, y=270)
+        self.test.place(x=260, y=280)
 
         self.finish=Button(window, text="Speed Up!", fg='white',bg='green',command=self.Finish)
-        self.finish.place(x=260, y=310)
+        self.finish.place(x=260, y=320)
         
         self.wpm_txt=Label(window, text="Enter Words Per Minute", fg='black', font=("Helvetica", 10))
-        self.wpm_txt.place(x=122, y=120)
+        self.wpm_txt.place(x=122, y=130)
         self.wpm=Entry(window, bg='white',fg='black', bd=5)
-        self.wpm.place(x=130, y=150)
+        self.wpm.place(x=130, y=160)
         
         self.languages=Listbox(window, height=5, selectmode='single')
         self.languages.insert(END,"English")
         self.languages.insert(END,"Hebrew")
         self.languages.insert(END,"French")
-        self.languages.place(x=320, y=150)
+        self.languages.place(x=320, y=160)
         self.languages.select_set(0) #first Language set as default 
         self.language_txt=Label(window, text="Video Language", fg='black', font=("Helvetica", 10))
-        self.language_txt.place(x=328, y=120)
-        
+        self.language_txt.place(x=328, y=130)
+
+ 
+    
+    def select_file(self):
+        filetypes = (('mp4 files', '*.mp4'), ('All files', '*.*'))
+        filename = fd.askopenfilename(title='Open a file', initialdir='/', filetypes=filetypes)
+        self.picked_file = True
+        self.file_path = filename
+
     def Finish (self):
         input_language = self.languages.get(self.languages.curselection()[0]) 
         input_wpm = int(self.wpm.get())
         print("Input Language: " + str(input_language) + "\n" + "Input WPM: " + str(input_wpm))
- 
+        
+        if not self.select_file:
+            print("Select File First")
+        else:
+            video = dynamic_speed.VideoEdit(self.file_path, input_language, input_wpm)  
+            clip_list = video.splitClips() 
+            video.update_speeds(clip_list)
+    
     def Test (self):
         input_wpm = int(self.wpm.get())
         print("Input WPM: " + str(input_wpm))
         #add 10 sec audio to test
     
-
 window=Tk()
 window.title('Video Speed Manager')
 window.geometry("600x400+10+20")
 mywin=MyWindow(window)
 window.mainloop()
+
+
 
 
 
