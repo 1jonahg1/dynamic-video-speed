a624fd4bff85caca52b345c3bc78dd4db9589750
Jonah <1jonahg1@gmail.com>
2022-10-02 11:03:07 +0300

    updated interface to work with test

--- interface.py
+++ interface.py
@@ -1,53 +1,55 @@
 from tkinter import *
 import tkinter.font as tkFont
 
-def B1FUNCTION ():
-    input_language = languages.get(languages.curselection()[0]) 
-    input_wpm = int(wpm.get())
-    print("Input Language: " + str(input_language) + "\n" + "Input WPM: " + str(input_wpm))
+class MyWindow:
+    def __init__(self, window):
+        window.iconbitmap('flash.ico')
+        self.title=Label(window, text="Change Lecture Speed to the Ideal Speed For You", fg='black', font=("Helvetica", 14))
+        self.title.pack()
+        self.f = tkFont.Font(self.title, self.title.cget("font"))
+        self.f.configure(underline = True)
+        self.title.configure(font=self.f)
+        self.title.place(x=75, y=45)
 
-def B2FUNCTION ():
-    input_wpm = int(wpm.get())
-    print("Input WPM: " + str(input_wpm))
-    #add 10 sec audio to test
+        self.test=Button(window, text="Test Speed", fg='white',bg='grey',command=self.Test)
+        self.test.place(x=260, y=270)
+
+        self.finish=Button(window, text="Speed Up!", fg='white',bg='green',command=self.Finish)
+        self.finish.place(x=260, y=310)
+        
+        self.wpm_txt=Label(window, text="Enter Words Per Minute", fg='black', font=("Helvetica", 10))
+        self.wpm_txt.place(x=122, y=120)
+        self.wpm=Entry(window, bg='white',fg='black', bd=5)
+        self.wpm.place(x=130, y=150)
+        
+        self.languages=Listbox(window, height=5, selectmode='single')
+        self.languages.insert(END,"English")
+        self.languages.insert(END,"Hebrew")
+        self.languages.insert(END,"French")
+        self.languages.place(x=320, y=150)
+        self.languages.select_set(0) #first Language set as default 
+        self.language_txt=Label(window, text="Video Language", fg='black', font=("Helvetica", 10))
+        self.language_txt.place(x=328, y=120)
+        
+    def Finish (self):
+        input_language = self.languages.get(self.languages.curselection()[0]) 
+        input_wpm = int(self.wpm.get())
+        print("Input Language: " + str(input_language) + "\n" + "Input WPM: " + str(input_wpm))
+ 
+    def Test (self):
+        input_wpm = int(self.wpm.get())
+        print("Input WPM: " + str(input_wpm))
+        #add 10 sec audio to test
+    
 
 window=Tk()
-window.iconbitmap('flash.ico')
-
-title=Label(window, text="Change Lecture Speed to the Ideal Speed For You", fg='black', font=("Helvetica", 14))
-
-title.pack()
-f = tkFont.Font(title, title.cget("font"))
-f.configure(underline = True)
-title.configure(font=f)
-title.place(x=75, y=45)
-
-test=Button(window, text="Test Speed", fg='white',bg='grey')
-test.place(x=260, y=270)
-test.bind('<Button-2>', B2FUNCTION)
-
-finish=Button(window, text="Speed Up!", fg='white',bg='green')
-finish.place(x=260, y=310)
-finish.bind('<Button-1>', B1FUNCTION)
-
-wpm_txt=Label(window, text="Enter Words Per Minute", fg='black', font=("Helvetica", 10))
-wpm_txt.place(x=122, y=120)
-wpm=Entry(window, bg='white',fg='black', bd=5)
-wpm.place(x=130, y=150)
-languages=Listbox(window, height=5, selectmode='single')
-languages.insert(END,"English")
-languages.insert(END,"Hebrew")
-languages.insert(END,"French")
-languages.place(x=320, y=150)
-languages.select_set(0) #first item as default
-
-
-language_txt=Label(window, text="Video Language", fg='black', font=("Helvetica", 10))
-language_txt.place(x=328, y=120)
-
 window.title('Video Speed Manager')
 window.geometry("600x400+10+20")
+mywin=MyWindow(window)
 window.mainloop()
+
+
+
 
 
 
