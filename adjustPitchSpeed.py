26216068476781e6de16fb34c95f52b897edfb50
Jonah <1jonahg1@gmail.com>
2022-10-07 13:56:13 +0300

    added linter

--- adjustPitchSpeed.py
+++ adjustPitchSpeed.py
@@ -2,43 +2,45 @@ import subprocess
 
 ffmpeg_exe = "ffmpeg.exe"
 
-COMMAND_BASE =  [ffmpeg_exe]
-COMMAND_BASE += ["-n"] # disable file overwriting
+COMMAND_BASE = [ffmpeg_exe]
+COMMAND_BASE += ["-n"]  # disable file overwriting
+
 
 class Video():
-  def __init__(self, path:str, speed:float=1.0):
-    self.path  = path
-    self.speed = speed
+    def __init__(self, path: str, speed: float = 1.0):
+        self.path = path
+        self.speed = speed
 
-def concatenate_videos(videos:list[Video], output_file:str):
-  video_count  = len(videos)
-  video_speeds = [float(1/x.speed) for x in videos]
-  audio_speeds = [float(x.speed) for x in videos]
 
-  cmd_input_files = []
-  filters, concat = ("", "")
-  for i, x in enumerate(videos):
-    cmd_input_files += ["-i", x.path]
-    filters += f"[{i}:v] setpts = {video_speeds[i]} * PTS [v{i}];"
-    filters += f"[{i}:a] atempo = {audio_speeds[i]}       [a{i}];"
-    concat  += f"[v{i}][a{i}]"
-  concat += f"concat = n = {video_count}:v = 1:a = 1 [v_all][a_all]"
+def concatenate_videos(videos: list[Video], output_file: str):
+    video_count = len(videos)
+    video_speeds = [float(1/x.speed) for x in videos]
+    audio_speeds = [float(x.speed) for x in videos]
 
-  filter_complex = f"{filters}{concat}".replace(" ", "")
+    cmd_input_files = []
+    filters, concat = ("", "")
+    for i, x in enumerate(videos):
+        cmd_input_files += ["-i", x.path]
+        filters += f"[{i}:v] setpts = {video_speeds[i]} * PTS [v{i}];"
+        filters += f"[{i}:a] atempo = {audio_speeds[i]}       [a{i}];"
+        concat += f"[v{i}][a{i}]"
+    concat += f"concat = n = {video_count}:v = 1:a = 1 [v_all][a_all]"
 
-  cmd_filter_complex = [
-    "-filter_complex", filter_complex,
-  ]
-  cmd_map = [
-    "-map", "[v_all]",
-    "-map", "[a_all]",
-  ]
-  command = sum([
-    COMMAND_BASE,
-    cmd_input_files,
-    cmd_filter_complex,
-    cmd_map,
-    [output_file],
-  ], [])
+    filter_complex = f"{filters}{concat}".replace(" ", "")
 
-  subprocess.run(command)
+    cmd_filter_complex = [
+        "-filter_complex", filter_complex,
+    ]
+    cmd_map = [
+        "-map", "[v_all]",
+        "-map", "[a_all]",
+    ]
+    command = sum([
+        COMMAND_BASE,
+        cmd_input_files,
+        cmd_filter_complex,
+        cmd_map,
+        [output_file],
+    ], [])
+
+    subprocess.run(command)
--- dynamic_speed.py
+++ dynamic_speed.py
@@ -1,5 +1,4 @@
-from email.mime import audio
-from moviepy.editor import *
+from moviepy.editor import VideoFileClip
 import speech_recognition as sr
 from adjustPitchSpeed import Video, concatenate_videos
 import os
@@ -7,61 +6,62 @@ import ffmpeg
 import math
 
 SEGMENT_DURATION = 30
-#WORDS_PER_MINUTE = 300
+# WORDS_PER_MINUTE = 300
 
-# loading video
+
 class VideoEdit():
-  def __init__(self, video_path, input_language, words_per_minute):
-    self.clip = VideoFileClip(video_path)  
-    self.duration = self.clip.duration
-    self.video_segments = math.ceil(self.duration / SEGMENT_DURATION)
-    self.last_video_duration = self.duration % SEGMENT_DURATION
-    self.language = input_language
-    self.wpm = words_per_minute
+    def __init__(self, video_path, input_language, words_per_minute):
+        self.clip = VideoFileClip(video_path)
+        self.duration = self.clip.duration
+        self.video_segments = math.ceil(self.duration / SEGMENT_DURATION)
+        self.last_video_duration = self.duration % SEGMENT_DURATION
+        self.language = input_language
+        self.wpm = words_per_minute
 
-  def splitClips(self):
-    clips = []
-    for i in range(self.video_segments-1):
-      clips.append(self.clip.subclip((i)*SEGMENT_DURATION, (i+1)*SEGMENT_DURATION))
-    clips.append(self.clip.subclip(self.duration-self.last_video_duration, self.duration))
-    for index, video in enumerate(clips):
-      video.write_videofile("dynamic_test_output/my_video_{}.mp4".format(index))
-    return clips
-  
-  def update_speeds(self, clip_list):
-    videos = []
-    test_words = []
-    for index in range(len(clip_list)-1):
-      words = get_words("my_video_{}".format(index))
-      test_words.append(words)
-      adjust_speed = self.wpm/(words*(60/SEGMENT_DURATION))
-      videos.append(Video(speed=adjust_speed, path="dynamic_test_output/my_video_{}.mp4".format(index))) # TODO - change to temp file
-    #edit last segment
-    words = get_words("my_video_{}".format(index+1))
-    adjust_speed = self.wpm/(words*(60/self.last_video_duration))
-    videos.append(Video(speed=adjust_speed, path="dynamic_test_output/my_video_{}.mp4".format(index+1)))
-    #update speeds 
-    concatenate_videos(videos=videos, output_file=f"dynamic_test_output/final_output_video.mp4")
-    
-    #test
-    for i, words in enumerate(test_words):
-      print('video {} - words: {}'.format(i,words))
+    def splitClips(self):
+        clips = []
+        for i in range(self.video_segments-1):
+            clips.append(self.clip.subclip((i)*SEGMENT_DURATION, (i+1)*SEGMENT_DURATION))
+        clips.append(self.clip.subclip(self.duration-self.last_video_duration, self.duration))
+        for index, video in enumerate(clips):
+            video.write_videofile("dynamic_test_output/my_video_{}.mp4".format(index))
+        return clips
+
+    def update_speeds(self, clip_list):
+        videos = []
+        test_words = []
+        for index in range(len(clip_list)-1):
+            words = get_words("my_video_{}".format(index))
+        test_words.append(words)
+        adjust_speed = self.wpm/(words*(60/SEGMENT_DURATION))
+        videos.append(Video(speed=adjust_speed,
+                            path="dynamic_test_output/my_video_{}.mp4".format(index)))  # TODO - change to temp file
+        # edit last segment
+        words = get_words("my_video_{}".format(index+1))
+        adjust_speed = self.wpm/(words*(60/self.last_video_duration))
+        videos.append(Video(speed=adjust_speed,
+                            path="dynamic_test_output/my_video_{}.mp4".format(index+1)))
+        # update speeds
+        concatenate_videos(videos=videos,
+                           output_file=f"dynamic_test_output/final_output_video.mp4")
+        # test
+        for i, words in enumerate(test_words):
+            print('video {} - words: {}'.format(i, words))
+
 
 def get_words(name):
-      command2mp3 = "ffmpeg -i dynamic_test_output/{}.mp4 dynamic_test_output/{}.mp3".format(name,name)
-      command2wav = "ffmpeg -i dynamic_test_output/{}.mp3 dynamic_test_output/{}.wav".format(name,name)
-      os.system(command2mp3)
-      os.system(command2wav)
-      r = sr.Recognizer()
-      with sr.AudioFile("dynamic_test_output/{}.wav".format(name)) as source:
-          audio = r.record(source, duration=SEGMENT_DURATION) 
-      text = (r.recognize_google(audio, language='he')).split() #language
-      print("!!!!Length of text!!!! \n" + str(len(text)))
-      return len(text)
-
-def test_update_audio(wpm, test_video_words = 56, test_video_duration = 19):
-  adjust_speed = wpm/(test_video_words*(60/test_video_duration))
-  #TODO - adjust the speed of audio (save as temp file?)
+    command2mp3 = "ffmpeg -i dynamic_test_output/{}.mp4 dynamic_test_output/{}.mp3".format(name, name)
+    command2wav = "ffmpeg -i dynamic_test_output/{}.mp3 dynamic_test_output/{}.wav".format(name, name)
+    os.system(command2mp3)
+    os.system(command2wav)
+    r = sr.Recognizer()
+    with sr.AudioFile("dynamic_test_output/{}.wav".format(name)) as source:
+        audio = r.record(source, duration=SEGMENT_DURATION)
+    text = (r.recognize_google(audio, language='he')).split()  # language
+    print("!!!!Length of text!!!! \n" + str(len(text)))
+    return len(text)
 
 
-
+def test_update_audio(wpm, test_video_words=56, test_video_duration=19):
+    adjust_speed = wpm/(test_video_words*(60/test_video_duration))
+    # TODO - adjust the speed of audio (save as temp file?)
